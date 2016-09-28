3c13f8df646658ac30c8bc26f6bfa74943587737
Zachary Weston <zakweston@zacharys-mbp.attlocal.net>
2016-09-27 21:48:05 -0400

    Second draft

--- db/migrate/20160925215554_create_books.rb
+++ db/migrate/20160925215554_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
       t.string :title, null: false, default: ''
-      t.integer :page_count, null: false, default: ''
+      t.integer :page_count, null: false, default: 0
       t.boolean :on_shelf, null: false, default: true
 
       t.timestamps null: false
